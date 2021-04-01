212caa14f107744a2d5bfd794c611e7e43680db5
Gustavo Moreira <m.muller.gustavo@gmail.com>
2021-04-01 18:37:39 -0300

    Implementando AFN -> AFD

--- AFN.py
+++ AFN.py
@@ -1,21 +1,49 @@
 class AFN:
-  # alfabeto = {'a', 'b', 'c'}
-  # estados = {'q0', 'q1', 'q2'}
-  # func_programa = {'q0': [('a', {'q1'}), ('b', {'q2'})]}
-  # estado_inicial = 'q0'
-  # estados_finais = {'qf'}
-  def __init__(self, alfabeto, estados, func_programa, estado_inicial, estados_finais):
-    
-    for key,value in enumerate(func_programa):
-        for transicao in func_programa[value]:
-            if transicao[0] == None:
-                raise ValueError("AFN nao pode possuir transicoes epsilon")
+    # alfabeto = {'a', 'b', 'c'}
+    # estados = {'q0', 'q1', 'q2'}
+    # func_programa = {'q0': [('a', {'q1'}), ('b', {'q2'})]}
+    # estado_inicial = 'q0'
+    # estados_finais = {'qf'}
+    def __init__(self, alfabeto, estados, func_programa, estado_inicial, estados_finais):
 
-    self.alfabeto = alfabeto
-    self.estados = estados
-    self.func_programa = func_programa
-    self.estado_inicial = estado_inicial
-    self.estados_finais = estados_finais
+        for key,value in enumerate(func_programa):
+            for transicao in func_programa[value]:
+                if transicao[0] == None:
+                    raise ValueError("AFN nao pode possuir transicoes epsilon")
 
-  def __str__(self):
-    return "alfabeto: "+str(self.alfabeto)+"\n"+"estados: "+str(self.estados)+"\n"+"func_programa: "+str(self.func_programa)+"\n"+"estado_inicial: "+str(self.estado_inicial)+"\n"+"estados_finais: "+str(self.estados_finais)
+        self.alfabeto = alfabeto
+        self.estados = estados
+        self.func_programa = func_programa
+        self.estado_inicial = estado_inicial
+        self.estados_finais = estados_finais
+
+    def __str__(self):
+        return "alfabeto: "+str(self.alfabeto)+"\n"+"estados: "+str(self.estados)+"\n"+"func_programa: "+str(self.func_programa)+"\n"+"estado_inicial: "+str(self.estado_inicial)+"\n"+"estados_finais: "+str(self.estados_finais)
+
+    #estados eh um set
+    def funcProgramaEstendida(self, estados, palavra):
+        if palavra == None:
+            return estados
+        
+        a = None
+        w = None
+
+        a = palavra[0]
+        w = palavra[1:]
+
+        if w == '':
+            w = None
+
+        resultado = set()
+
+        for estado in estados:
+            if estado not in self.estados:
+                raise Exception("Estado inexistente")
+
+            if estado in self.func_programa:
+                transicoes = self.func_programa[estado]
+                for transicao in transicoes:
+                    if transicao[0] == a:
+                        resultado = resultado.union(transicao[1])
+
+        return self.funcProgramaEstendida(resultado, w)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from AFNe import *
 from AFN import *
+from AFD import *
 
 alfabeto_er = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
 
@@ -151,7 +152,8 @@ def erToAFNe(er):
 
     return pilha.pop()
 
-def afneToAFD(afne):
+#Acho que o nome dessa funcao estah errado no enunciado
+def afneToAFN(afne):
 
     alfabeto = afne.alfabeto.copy()
     estados = afne.estados.copy()
@@ -177,8 +179,107 @@ def afneToAFD(afne):
 
     return AFN(alfabeto, estados, func_programa, estado_inicial, estados_finais)
 
-# print(erToAFNe("*(+(a,b))"))
 
-afne = AFNe({'a', 'b'}, {'q0', 'q1', 'q2'}, {'q0': [(None, {'q1'}), ('a', {'q0'})], 'q1': [(None, {'q2'}), ('b', {'q1'})], 'q2': [('a', {'q2'})]}, 'q0', {'q2'})
+def afntoAFD(afn):
 
-print(afneToAFD(afne))
+    def string_to_set(conjunto):
+        return set(conjunto.replace('{','').replace('}','').replace(' ','').split(','))
+
+    #key eh um conjunto e func_programa possui stringfied sets como chaves
+    def list_has_set(lista, conjunto):
+        for value in lista:
+            key_set = string_to_set(value)
+            if key_set == conjunto:
+                return str(key_set)
+
+        return False
+
+    alfabeto = afn.alfabeto.copy()
+    estados = set()
+    func_programa = {}
+    estado_inicial = None
+    estados_finais = set()
+
+    explorado = []
+    explorar = [{afn.estado_inicial}]
+
+    todas_transicoes = []
+
+    while len(explorar) > 0:
+        e = explorar.pop(0)
+        explorado.append(e)
+
+        for letra in alfabeto:
+            resultado = afn.funcProgramaEstendida(e,letra)
+            if len(resultado) > 0:
+                todas_transicoes.append([e.copy(), letra, resultado])
+                if resultado not in explorado:
+                    explorar.append(resultado)
+
+    #Renomeando todos os estados
+    nome_estado_contador = 0
+    for transicao in todas_transicoes:
+        estado = transicao[0]
+        destino = transicao[2]
+        
+        if isinstance(estado, set): #Caso nao tenha sido convertido ainda
+
+            estados.add('q'+str(nome_estado_contador))
+
+            if not estado.isdisjoint(afn.estados_finais):
+                estados_finais.add('q'+str(nome_estado_contador))
+
+            if list(estado)[0] == afn.estado_inicial and estado_inicial == None:
+                print(estado, afn.estado_inicial)
+                estado_inicial = 'q'+str(nome_estado_contador)
+
+            for t in todas_transicoes:
+                if t[0] == estado:
+                    t[0] = 'q'+str(nome_estado_contador)
+                if t[2] == estado:
+                    t[2] = 'q'+str(nome_estado_contador)
+            nome_estado_contador += 1
+
+        if estado != destino:
+
+            if isinstance(destino, set): #Caso nao tenha sido convertido ainda
+
+                estados.add('q'+str(nome_estado_contador))
+
+                if not destino.isdisjoint(afn.estados_finais):
+                    estados_finais.add('q'+str(nome_estado_contador))
+
+                if list(destino)[0] == afn.estado_inicial and estado_inicial == None:
+                    print(list(destino)[0], afn.estado_inicial)
+                    estado_inicial = 'q'+str(nome_estado_contador)
+
+                for t in todas_transicoes:
+                    if t[0] == destino:
+                        t[0] = 'q'+str(nome_estado_contador)
+                    if t[2] == destino:
+                        t[2] = 'q'+str(nome_estado_contador)
+                nome_estado_contador += 1
+
+    for transicao in todas_transicoes:
+        estado = transicao[0]
+        termo = transicao[1]
+        destino = transicao[2]
+
+        if estado not in func_programa:
+            func_programa[estado] = [(termo, {destino})]
+        else:
+            func_programa[estado].append((termo, {destino}))
+
+    return AFD(alfabeto, estados, func_programa, estado_inicial, estados_finais)
+
+func_programa = {
+                    'q0': [('a', {'q0','q1'}), ('b', {'q0'})],
+                    'q1': [('a', {'q2'})],
+                    'q2': [('a', {'qf'})]
+                }
+
+afn = AFN({'a','b'}, {'q0','q1','q2','qf'}, func_programa, 'q0', {'qf'})
+
+# print(afn.funcProgramaEstendida({'q0'}, "c"))
+print(afntoAFD(afn))
+# set(a.replace('{','').replace('}','').replace(' ','').split(','))
