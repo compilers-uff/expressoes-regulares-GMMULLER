642aa4a7e2443d84ced3e6ac750985a15dc68e35
Gustavo Moreira <m.muller.gustavo@gmail.com>
2021-04-11 12:53:21 -0300

    Comentando classe AFNe

--- AFNe.py
+++ AFNe.py
@@ -1,9 +1,12 @@
 class AFNe:
-  # alfabeto = {'a', 'b', 'c'}
-  # estados = {'q0', 'q1', 'q2'}
-  # func_programa = {'q0': [('a', {'q1'}), ('b', {'q2'})]}
-  # estado_inicial = 'q0'
-  # estados_finais = {'qf'}
+
+  '''
+    @param set    alfabeto
+    @param set    estados
+    @param dict   func_programa Exemplo: {'q0': [('a', {'q1'}), ('b', {'q2'})]}.
+    @param string estado_inicial
+    @param set    estados_finais
+  '''
   def __init__(self, alfabeto, estados, func_programa, estado_inicial, estados_finais):
     self.alfabeto = alfabeto
     self.estados = estados
@@ -14,19 +17,25 @@ class AFNe:
   def __str__(self):
     return "alfabeto: "+str(self.alfabeto)+"\n"+"estados: "+str(self.estados)+"\n"+"func_programa: "+str(self.func_programa)+"\n"+"estado_inicial: "+str(self.estado_inicial)+"\n"+"estados_finais: "+str(self.estados_finais)
 
-  #mudar nome para accepted
-  def aceita(self, palavra):
+  '''
+    Retorna False ou True dado uma palavra de acordo com sua aceitacao pelo automato.
+
+    @param string w
+  '''
+  def accepted(self, w):
     
-    estados_alcancados = self.funcProgramaEstendida(self.estado_inicial, palavra)
+    estados_alcancados = self.deltaEstrela(self.estado_inicial, w)
 
     if estados_alcancados.isdisjoint(estados_finais):
       return False
     else:
       return True
 
-  #renomear funcao para delta*
-  #estados eh um set
-  def funcProgramaEstendida(self, estados, palavra):
+  '''
+    @param set    estados
+    @param string palavra
+  '''
+  def deltaEstrela(self, estados, palavra):
     if palavra == None:
       return self.fechoVazioEstendido(estados)
 
@@ -38,7 +47,7 @@ class AFNe:
 
     a = palavra[-1] #ultimo caracter
 
-    conjunto_S = self.funcProgramaEstendida(estados, w)
+    conjunto_S = self.deltaEstrela(estados, w)
     conjunto_R = set()
 
     for s in conjunto_S:
@@ -50,7 +59,11 @@ class AFNe:
     
     return self.fechoVazioEstendido(conjunto_R)
 
-  # Recebe um conjunto e retorna um conjunto (set)
+  '''
+    Calcula o fecho vazio para um conjunto de estados.
+
+    @param set estados
+  '''
   def fechoVazioEstendido(self, estados):
 
     retorno = set()
@@ -60,8 +73,9 @@ class AFNe:
 
     return retorno
 
-  # Recebe um estado (nao pode ser um conjunto) e retorna um conjunto (set)
-  # Recursivo causa iteracoes infinitas se houver loop de transicoes vazias
+  '''
+    @param string estado
+  '''
   def fechoVazio(self, estado):
     
     retorno = set()
@@ -69,6 +83,7 @@ class AFNe:
     explorado = set()
     explorar = [estado]
 
+    #Faz uma busca em largura porque recursao pode resultar num loop infinito
     while len(explorar) > 0:
       e = explorar[0]
       retorno.add(e)
@@ -78,7 +93,7 @@ class AFNe:
 
         for p in possibilidades:
           if p[0] == None:
-            if explorado == set(): #se explorado ainda estah vazio
+            if explorado == set(): #se explorado ainda esta vazio
               for a in p[1]: #o que ainda nao foi explorado
                 explorar.append(a)
             else:
